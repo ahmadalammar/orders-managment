45d1971b701d0d6bdcb58124467ccf772b29b0e6
“Alammar” <ahmad1costi@gmail.com>
2022-12-26 15:21:23 +0800

    first commit

---
+++ payment-service/src/test/java/com/alammar/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alammar.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product-service/src/test/java/com/alammar/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alammar.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
